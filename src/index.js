50f2114a270077720b698ba7b4522b18d0c27803
Erika Gutierrez <erikagutierrez01@gmail.com>
2017-01-21 14:32:21 -0400

    postgresql

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+    console.log(`Feathers application started on ${app.get('host')}:${process.env.PORT}`)
 );
